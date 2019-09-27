54f9d4e19189f3155289dd42cf4887ed7c6a8922
Ilton <xlsonplay@gmail.com>
2019-09-27 16:29:24 -0300

    Cria tela

--- src/br/com/exerc1/ListaPresenca.java
+++ src/br/com/exerc1/ListaPresenca.java
@@ -4,5 +4,18 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ListaPresenca extends JFrame{
-	
+	private static final long serialVersionUID = 1L;
+
+
+
+	public static void main(String[] args){
+		
+		ListaPresenca janela = new ListaPresenca();
+		
+		janela.setSize(270,340);
+		janela.setVisible( true );
+		
+		new ListaPresenca();
+
+		}
 }
