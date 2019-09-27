baa5713d46440db4dd75dfdac9cbe3d8dca69aba
Ilton <xlsonplay@gmail.com>
2019-09-27 16:34:25 -0300

    Adiciona Checkbox

--- src/br/com/exerc1/ListaPresenca.java
+++ src/br/com/exerc1/ListaPresenca.java
@@ -3,27 +3,23 @@ package br.com.exerc1;
 import java.awt.*;
 import javax.swing.*;
 
-public class ListaPresenca extends JFrame{
+public class ListaPresenca<E> extends JFrame{
 	private static final long serialVersionUID = 1L;
 
 
 	ListaPresenca(){   
 		JFrame frame = new JFrame("Lista de presença");
-		JButton button,button1, button2, button3,button4;
-		button = new JButton("button 1");
-		button1 = new JButton("button 2");
-		button2 = new JButton("button 3");
-		button3 = new JButton("button 4");
-		button4 = new JButton("button 5");
+		JButton button;
+		JCheckBox checkbox;
+		button = new JButton("Adiciona texto");
+		checkbox = new JCheckBox("Nome");
+		
 		frame.add(button);
-		frame.add(button1);
-		frame.add(button2);
-		frame.add(button3);
-		frame.add(button4);
+		frame.add(checkbox);
 		frame.setLayout(new FlowLayout());
 		frame.setSize(300,300);  
 		frame.setVisible(true);  
-
+		
 
 	}
 	public static void main(String[] args){
